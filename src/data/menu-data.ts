d398816787017006878eb5c9f345131ae8c45dc0
karan dhole <karan.dhole20@pccoepune.org.>
2025-09-25 19:19:13 +0530

    n

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -69,42 +69,42 @@ const menu_data:IMenuData[] = [
   {
     id:3,
     name:'Blog',
-    link:'/blog',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Big image',link:'/blog'},
-      {title:'Right sidebar',link:'/blog-right-sidebar'},
-      {title:'Left sidebar',link:'/blog-left-sidebar'},
-      {title:'Single Post',link:'/blog-details'},
-    ]
+    link:'/',
+    // has_dropdown:true,
+    // dropdown_menus:[
+    //   {title:'Big image',link:'/blog'},
+    //   {title:'Right sidebar',link:'/blog-right-sidebar'},
+    //   {title:'Left sidebar',link:'/blog-left-sidebar'},
+    //   {title:'Single Post',link:'/blog-details'},
+    // ]
   },
-  {
-    id:4,
-    name:'Pages',
-    link:'/about',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Shop Location One',link:'/shop-location'},
-      {title:'Shop Location Two',link:'/shop-location-2'},
-      {title:'FAQs',link:'/faq'},
-      {title:'Checkout',link:'/checkout'},
-      {title:'Cart Page',link:'/cart'},
-      {title:'Compare Page',link:'/compare'},
-      {title:'Wishlist',link:'/wishlist'},
-      {title:'Sign In',link:'/login'},
-      {title:'Coming soon',link:'/coming-soon'},
-      {title:'Page 404',link:'/404'},
-    ]
-  },
+  // {
+  //   id:4,
+  //   name:'Pages',
+  //   link:'',
+  //   has_dropdown:true,
+  //   dropdown_menus:[
+  //     {title:'Shop Location One',link:'/shop-location'},
+  //     {title:'Shop Location Two',link:'/shop-location-2'},
+  //     {title:'FAQs',link:'/faq'},
+  //     {title:'Checkout',link:'/checkout'},
+  //     {title:'Cart Page',link:'/cart'},
+  //     {title:'Compare Page',link:'/compare'},
+  //     {title:'Wishlist',link:'/wishlist'},
+  //     {title:'Sign In',link:'/login'},
+  //     {title:'Coming soon',link:'/coming-soon'},
+  //     {title:'Page 404',link:'/404'},
+  //   ]
+  // },
   {
     id:5,
     name:'About Us',
-    link:'/about',
+    link:'/',
   },
   {
     id:6,
     name:'Contact Us',
-    link:'/contact',
+    link:'/',
   },
 ]
 
@@ -116,79 +116,79 @@ export const mobile_menus = [
     id:1,
     name:'Home',
     link:'/',
-    has_dropdown:true,
-    home_menus:[
-      {title:'Home Page V1',img:'/assets/img/header/home1-1.jpg',link:'/'},
-      {title:'Home Page V2',img:'/assets/img/header/home2-1.jpg',link:'/home-2'},
-      {title:'Home Page V3',img:'/assets/img/header/home3-1.jpg',link:'/home-3'},
-      {title:'Home Page V4',img:'/assets/img/header/home4-1.jpg',link:'/home-4'},
-      {title:'Home Page V5',img:'/assets/img/header/home5-1.jpg',link:'/home-5'},
-      {title:'Home Page V6',img:'/assets/img/header/home6-1.jpg',link:'/home-6'},
-    ]
+    // has_dropdown:true,
+    // home_menus:[
+    //   {title:'Home Page V1',img:'/assets/img/header/home1-1.jpg',link:'/'},
+    //   {title:'Home Page V2',img:'/assets/img/header/home2-1.jpg',link:'/home-2'},
+    //   {title:'Home Page V3',img:'/assets/img/header/home3-1.jpg',link:'/home-3'},
+    //   {title:'Home Page V4',img:'/assets/img/header/home4-1.jpg',link:'/home-4'},
+    //   {title:'Home Page V5',img:'/assets/img/header/home5-1.jpg',link:'/home-5'},
+    //   {title:'Home Page V6',img:'/assets/img/header/home6-1.jpg',link:'/home-6'},
+    // ]
   },
   {
     id:2,
     name:'Shop',
-    link:'/shop',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Shop Left sidebar',link:'/shop'},
-      {title:'Shop Without Banner',link:'/shop-2'},
-      {title:'Shop Version',link:'/shop-3'},
-      {title:'Shop Right sidebar',link:'/shop-right'},
-      {title:'Shop List view',link:'/shop-list'},
-    ]
+    link:'/shop-2',
+    // has_dropdown:true,
+    // dropdown_menus:[
+    //   {title:'Shop Left sidebar',link:'/shop'},
+    //   {title:'Shop Without Banner',link:'/shop-2'},
+    //   {title:'Shop Version',link:'/shop-3'},
+    //   {title:'Shop Right sidebar',link:'/shop-right'},
+    //   {title:'Shop List view',link:'/shop-list'},
+    // ]
   },
-  {
-    id:3,
-    name:'Shop Details',
-    link:'/shop-details',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Image scroll',link:'/shop-details'},
-      {title:'Product grid',link:'/shop-details-2'},
-      {title:'Top Thumb Product',link:'/shop-details-3'},
-      {title:'Simple Product',link:'/shop-details'},
-    ]
-  },
+  // {
+  //   id:3,
+  //   name:'Shop Details',
+  //   link:'/shop-details',
+  //   has_dropdown:true,
+  //   dropdown_menus:[
+  //     {title:'Image scroll',link:'/shop-details'},
+  //     {title:'Product grid',link:'/shop-details-2'},
+  //     {title:'Top Thumb Product',link:'/shop-details-3'},
+  //     {title:'Simple Product',link:'/shop-details'},
+  //   ]
+  // },
   {
     id:4,
     name:'Blog',
-    link:'/blog',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Big image',link:'/blog'},
-      {title:'Right sidebar',link:'/blog-right-sidebar'},
-      {title:'Left sidebar',link:'/blog-left-sidebar'},
-      {title:'Single Post',link:'/blog-details'},
-    ]
+    link:'/',
+    // has_dropdown:true,
+    // dropdown_menus:[
+    //   {title:'Big image',link:'/blog'},
+    //   {title:'Right sidebar',link:'/blog-right-sidebar'},
+    //   {title:'Left sidebar',link:'/blog-left-sidebar'},
+    //   {title:'Single Post',link:'/blog-details'},
+    // ]
   },
-  {
-    id:5,
-    name:'Pages',
-    link:'/about',
-    has_dropdown:true,
-    dropdown_menus:[
-      {title:'Shop Location One',link:'/shop-location'},
-      {title:'Shop Location Two',link:'/shop-location-2'},
-      {title:'FAQs',link:'/faq'},
-      {title:'Checkout',link:'/checkout'},
-      {title:'Cart Page',link:'/cart'},
-      {title:'Compare Page',link:'/compare'},
-      {title:'Wishlist',link:'/wishlist'},
-      {title:'Sign In',link:'/login'},
-      {title:'Coming soon',link:'/coming-soon'},
-      {title:'Page 404',link:'/404'},
-    ]
-  },
+  // {
+  //   id:5,
+  //   name:'Pages',
+  //   link:'/about',
+  //   has_dropdown:true,
+  //   dropdown_menus:[
+  //     {title:'Shop Location One',link:'/shop-location'},
+  //     {title:'Shop Location Two',link:'/shop-location-2'},
+  //     {title:'FAQs',link:'/faq'},
+  //     {title:'Checkout',link:'/checkout'},
+  //     {title:'Cart Page',link:'/cart'},
+  //     {title:'Compare Page',link:'/compare'},
+  //     {title:'Wishlist',link:'/wishlist'},
+  //     {title:'Sign In',link:'/login'},
+  //     {title:'Coming soon',link:'/coming-soon'},
+  //     {title:'Page 404',link:'/404'},
+  //   ]
+  // },
   {
     id:6,
     name:'About Us',
-    link:'/about',
+    link:'/',
   },
   {
     id:7,
     name:'Contact Us',
-    link:'/contact',
+    link:'/',
   },
 ]
