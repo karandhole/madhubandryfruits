c3bf8790e0cf1e50c937e4376678b97955b6d1dc
karan dhole <karan.dhole20@pccoepune.org.>
2025-12-18 17:28:06 +0530

    S

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',     // enables static export (next build + next export)
-  images: {
-    unoptimized: true,  // disables Next.js image optimization
-  },
+  // output: 'export',     // enables static export (next build + next export)
+  // images: {
+  //   unoptimized: true,  // disables Next.js image optimization
+  // },
 };
 
 module.exports = nextConfig;
